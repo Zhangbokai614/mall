c7068d5794579cb085012faef5fe1ca8ca66a017
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2021-10-13 14:30:04 +0800

    add vant-weapp directory

--- src/mock/db.js
+++ src/mock/db.js
@@ -1,19 +1,37 @@
-const  Mock = require('mockjs')
+const Mock = require('mockjs')
 
 const Random = Mock.Random;
 
+function classItem() {
+    const classItem = Mock.mock({
+        "item|1-5": [
+            "综合推荐",
+            "生鲜",
+            "水果",
+            "米面粮油",
+            "特色产品"
+        ]
+    })
+    console.log(classItem)
+    return classItem.item[classItem.item.length - 1]
+}
+
 module.exports = () => {
+
+    console.log(classItem.item)
+
     const data = Mock.mock({
-            "test|1-100": [
-                {
+        "test|1-100": [
+            {
                 "id|+1": 1,
                 "title": '@csentence',
+                "classItem": classItem(),
                 "inventory|0-10000": 10000,
                 images: Random.image('200x100', '#4A7BF7', 'Mock'),
                 "price|10-80": 80,
                 "marketPrice|15-100": 100
-                }
-            ]
-        })
+            }
+        ]
+    })
     return data
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,55 @@
 import React, { Component } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { AtButton, AtTabs, AtTabsPane } from 'taro-ui'
 
 import './index.css'
 
 export default class Index extends Component {
-  render () {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      current: 0,
+    }
+  }
+  handleClick(value) {
+    this.setState({
+      current: value
+    })
+  }
+
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
-        <Text>Taro UI 支持 Vue 了吗？</Text>
-        <AtButton type='primary' circle={true}>支持</AtButton>
-        <Text>共建？</Text>
-        <AtButton type='secondary' circle={true}>来</AtButton>
-      </View>
+      <AtTabs
+        current={this.state.current}
+        scroll
+        tabList={[
+          { title: '标签页1' },
+          { title: '标签页2' },
+          { title: '标签页3' },
+          { title: '标签页4' },
+          { title: '标签页5' },
+          { title: '标签页6' }
+        ]}
+        onClick={this.handleClick.bind(this)}>
+        <AtTabsPane current={this.state.current} index={0}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页一的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={1}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页二的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={2}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页三的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={3}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页四的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={4}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页五的内容</View>
+        </AtTabsPane>
+        <AtTabsPane current={this.state.current} index={5}>
+          <View style='font-size:18px;text-align:center;height:100px;'>标签页六的内容</View>
+        </AtTabsPane>
+      </AtTabs>
     )
   }
 }
