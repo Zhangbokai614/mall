4d520ce3a09352742c7f2d99c4a4afb745426013
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2021-10-19 15:22:36 +0800

    tabBar

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,7 @@
 export default {
   pages: [
     'pages/home/index',
+    'pages/cart/index',
     'pages/detail/index'
   ],
 
@@ -9,5 +10,24 @@ export default {
     navigationBarBackgroundColor: '#ff4949',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'white'
+  },
+
+  tabBar: {
+    color: 'rgba(68, 68, 68, 1)',
+    selectedColor: '#ff4949',
+    backgroundColor: 'white',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: 'home',
+        iconPath: './asset/images/tabs/home.png',
+        selectedIconPath: './asset/images/tabs/home-select.png'
+      },
+      {
+        pagePath: 'pages/cart/index',
+        text: 'card',
+        iconPath: './asset/images/tabs/shopping-cart.png',
+        selectedIconPath: './asset/images/tabs/shopping-cart-select.png'
+      }]
   }
 }
