639b84208d7275a987f5037e2dcc10f3865f7fcc
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2021-10-20 14:59:41 +0800

    cart

--- src/app.config.js
+++ src/app.config.js
@@ -1,35 +1,30 @@
 export default {
-  pages: [
-    'pages/detail/index',
-    'pages/home/index',
-    'pages/cart/index'
-    
-  ],
+  pages: ["pages/home/index", "pages/cart/index"],
 
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#ff4949',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'white'
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#ff4949",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "white"
   },
 
   tabBar: {
-    color: 'rgba(68, 68, 68, 1)',
-    selectedColor: '#ff4949',
-    backgroundColor: 'white',
+    color: "rgba(68, 68, 68, 1)",
+    selectedColor: "#ff4949",
+    backgroundColor: "white",
     list: [
       {
-        pagePath: 'pages/home/index',
-        text: 'home',
-        iconPath: './asset/images/tabs/home.png',
-        selectedIconPath: './asset/images/tabs/home-select.png'
+        pagePath: "pages/home/index",
+        text: "home",
+        iconPath: "./asset/images/tabs/home.png",
+        selectedIconPath: "./asset/images/tabs/home-select.png"
       },
       {
-        pagePath: 'pages/cart/index',
-        text: 'cart',
-        iconPath: './asset/images/tabs/shopping-cart.png',
-        selectedIconPath: './asset/images/tabs/shopping-cart-select.png'
-      }]
+        pagePath: "pages/cart/index",
+        text: "cart",
+        iconPath: "./asset/images/tabs/shopping-cart.png",
+        selectedIconPath: "./asset/images/tabs/shopping-cart-select.png"
+      }
+    ]
   }
-  
-}
+};
