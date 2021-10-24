94edc7325b3062efa0e0997a601b59ba38cc7883
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2021-10-24 15:51:56 +0800

    cart

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,2 @@
 // Link prefix
-<<<<<<< HEAD
 export const baseUrl = "http://192.168.0.39:3003";
-=======
-export const baseUrl = 'http://192.168.0.39:3003';
->>>>>>> d480d92b0b0b0807406b6c357a0d75b066189fd9
