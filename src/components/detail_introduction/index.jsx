677e73de4b374b54388239f220323dad3ec83d6a
shuaiqidechuan <1335917149@qq.com>
2021-10-27 16:27:58 +0800

    nothing

--- src/components/detail_introduction/index.jsx
+++ src/components/detail_introduction/index.jsx
@@ -1,7 +1,8 @@
-import { View } from '@tarojs/components'
+import { View, Image } from '@tarojs/components'
 import React from 'react'
 import { AtDivider } from 'taro-ui'
 
+import Vertical from '../../asset/images/icon/vertical.png'
 import './index.css'
 import { Get } from '../../global-data/index'
 
@@ -10,15 +11,21 @@ class Introduction extends React.Component {
     render() {
 
         return (
-            
+
             <View className='detailIntroduce'>
                 <View className='goodsIntroduce'>
+                    <Image
+                        className="vertical"
+                        src={Vertical}
+                        style='width:2vw; height:9vw;'
+                    >
+                    </Image>
                     <View className='introduceTitle'>
                         {Get('languages').goodsIntroduce}
                     </View>
                 </View>
                 <AtDivider />
-                
+
             </View>
         )
     }
